4e783e416410bade6b08c49a8e93f4e298d176e8
Dipun Mistry <dmistry@opentable.com>
2020-05-24 06:17:13 +0100

    heroku

--- server/index.ts
+++ server/index.ts
@@ -1,11 +1,22 @@
+import path from 'path';
+import express from 'express';
+import http from 'http';
 import socketio from 'socket.io';
 
-const io = socketio();
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+var port = process.env.PORT || 8888;
+
+
+app.use(express.static(path.join(__dirname, '../build')))
 
 io.on('connection', socket => {
     console.log('Hey! a connection', socket.id);
-    socket.on('chat', data => io.emit('chat', data))
+
+    socket.on('chat', data => io.emit('chat', data));
 });
 
-io.listen(8888);
-console.log('socket.io listening on port 8888');
+io.listen(port);
+console.log(`socket.io listening on port ${port}`);

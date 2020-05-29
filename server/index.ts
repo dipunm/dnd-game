19e7072e2a57b706d6bf5f983d23a9be19a11730
Dipun Mistry <dmistry@opentable.com>
2020-05-29 23:56:28 +0100

    separated db code and added stricter types

--- server/index.ts
+++ server/index.ts
@@ -2,39 +2,28 @@ import path from 'path';
 import express from 'express';
 import http from 'http';
 import socketio from 'socket.io';
-import mongoose from 'mongoose';
+
+import { ChatMsg } from './mongo';
 
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
-const uri: string = process.env.MONGODB_URI || "mongodb://Test:iMc5Ln5P0xQS2OcOK@ds149433.mlab.com:49433/heroku_cfk6wf32";
 
 var port = process.env.PORT || 8888;
 
-
 app.use(express.static(path.join(__dirname, '../build')))
 
-mongoose.connect(uri, (err: any) => {
-    if (err) {
-        console.log("Mongo Error:" + err.message);
-    }
-    else
-    {
-        console.log("Successfully connected to the database.");
-    }
-});
+io.on('connection', socket => {    
+    socket.on('chat-init', (callback: (messages: Message[]) => void) => {
+        // Send back the database, when ChatObservable emits this event
+        ChatMsg.find((err, chatMsgs) => {
+            if (err) return console.error(err);
+            const messages = chatMsgs.map<Message>((value) => value.toJSON());
+            callback(messages); // acts like return
+        });
+    });
 
-// Set up database variables
-const chatMsgSchema = new mongoose.Schema({
-    handle: String,
-    message: String
-});
-const ChatMsg = mongoose.model('ChatMsg', chatMsgSchema);
-
-io.on('connection', socket => {
-    console.log('Hey! a connection', socket.id);
-    
-    socket.on('chat', data => {
+    socket.on('chat', (data: Message) => {
         io.emit('chat', data); // Send the chat message to every other browser
         // Save chat message to the database
         var chatMsg = new ChatMsg(data);
@@ -42,15 +31,6 @@ io.on('connection', socket => {
             if (err) return console.error(err); // Should I use this, or console.log(err.message)?
         });
     });
-
-    socket.on('chat-init', (callback) => {
-        // Send back the database, when ChatObservable emits this event
-        ChatMsg.find({},{_id: 0, handle: 1, message: 1},(err, chatMsgs) => {
-            if (err) return console.error(err);
-            callback(chatMsgs.map((value) => value.toJSON())); // acts like return
-        });
-    });
-
 });
 
 server.listen(port, () => {
---
+++ server/mongo.ts
@@ -0,0 +1,26 @@
+import mongoose from 'mongoose';
+
+// This will prevent the warnings.
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+mongoose.set('useUnifiedTopology', true);
+
+const uri: string = process.env.MONGODB_URI || "mongodb://Test:iMc5Ln5P0xQS2OcOK@ds149433.mlab.com:49433/heroku_cfk6wf32";
+mongoose.connect(uri, (err: any) => {
+    if (err) {
+        console.log("Mongo Error:" + err.message);
+    }
+    else
+    {
+        console.log("Successfully connected to the database.");
+    }
+});
+
+// Set up database variables
+const chatMsgSchema = new mongoose.Schema({
+    handle: String,
+    message: String
+});
+type MessageDoc = Message & mongoose.Document;
+export const ChatMsg = mongoose.model<MessageDoc>('ChatMsg', chatMsgSchema);
